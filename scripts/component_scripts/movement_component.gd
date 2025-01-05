d7c9a355ee656fb64af2ac0a9636f5070fea68a4
DavidDomenzain <s231931@dtu.dk>
2025-01-05 10:54:11 -0600

    Added testing tilemap and acceleration and acceleration for both air and ground movement

--- scripts/component_scripts/movement_component.gd
+++ scripts/component_scripts/movement_component.gd
@@ -3,6 +3,17 @@ class_name MovementComponent
 
 @export_subgroup("Settings")
 @export var speed: float = 100.0
+@export var ground_accel_speed: float = 6.0
+@export var ground_decel_speed: float = 20.0
+@export var air_accel_speed: float = 10.0
+@export var air_decel_speed: float = 3.0
+
 
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
-	body.velocity.x = direction * speed
+	var velocity_change_speed: float = 0.0
+	if body.is_on_floor():
+		velocity_change_speed = ground_accel_speed if direction != 0 else ground_decel_speed
+	else:
+		velocity_change_speed = air_accel_speed if direction != 0 else air_decel_speed
+	
+	body.velocity.x = move_toward(body.velocity.x, direction * speed, velocity_change_speed)
